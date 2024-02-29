bec3e8ea7fc13f11179084472acd0a1332cf4ddf
pfigithub <pampam7072@gmail.com>
2024-02-29 15:07:29 +0330

    reading data

--- dbscn.py
+++ dbscn.py
@@ -4,3 +4,8 @@ from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler 
 import matplotlib.pyplot as plt 
 
+# reading
+import pandas as pd
+
+pdf = pd.read_csv('youdata')
+pdf.head(5)
